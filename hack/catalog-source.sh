6744cb856acdb36cd4c7a452500722d0f6798124
Aneesh Puttur <aneeshputtur@gmail.com>
2019-09-24 15:38:35 -0400

    Added catalog-source

---
+++ hack/catalog-source.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [[ -z ${1} ]]; then
+    CATALOG_NS="marketplace"
+else
+    CATALOG_NS=${1}
+fi
+
+CSV=`cat deploy/olm-catalog/silicon-pod-operator/0.0.1/silicon-pod-operator.v0.0.1.clusterserviceversion | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
+CRD=`cat deploy/crds/app_v1alpha1_siliconpod_crd.yaml  | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
+PKG=`cat deploy/olm-catalog/silicon-pod-operator/silicon-pod-operator.package.yaml | sed -e 's/^/          /' | sed '0,/ /{s/          /        - /}'`
+
+cat << EOF > deploy/olm-catalog/silicon-pod-operator/0.1.0/catalog-source.yaml
+apiVersion: v1
+kind: List
+items:
+  - apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: silicon-pod-operator-resources
+      namespace: ${CATALOG_NS}
+    data:
+      clusterServiceVersions: |
+${CSV}
+      customResourceDefinitions: |
+${CRD}
+      packages: >
+${PKG}
+
+  - apiVersion: operators.coreos.com/v1alpha1
+    kind: CatalogSource
+    metadata:
+      name: silicon-pod-operator-resources
+      namespace: ${CATALOG_NS}
+    spec:
+      configMap: silicon-pod-operator-resources
+      displayName: Silicon Pod Operators
+      publisher: Red Hat
+      sourceType: internal
+    status:
+      configMapReference:
+        name: silicon-pod-operator-resources
+        namespace: ${CATALOG_NS}
+EOF
